6e172e29ce0046ecea89842e6ee1d533c6c7e652
Fyrna N. <imfyrna@gmail.com>
2025-08-11 04:29:10 +0700

    just make code more readable

--- prompt.go
+++ prompt.go
@@ -14,30 +14,38 @@ import (
 
 // keys
 const (
-	keyArrowUp   = "\x1b[A"
-	keyArrowDown = "\x1b[B"
-	keyEnter     = "\r"
-	keySpace     = " "
-	keyBackspace = "\x7f"
-	keyCtrlC     = 3
-	keyCtrlQ     = 17
+	keyArrowUp    = "\x1b[A"
+	keyArrowDown  = "\x1b[B"
+	keyEnter      = "\r"
+	keySpace      = " "
+	keyBackspace1 = 0x7f
+	keyBackspace2 = 0x08
+	keyCtrlC      = 0x03
+	keyCtrlQ      = 0x11
 )
 
 // terminal wrapper
 type terminal struct {
-	fd       int
-	oldState *term.State
+	fd, width, height int
+	oldState          *term.State
 }
 
-func newTerminal() *terminal {
-	return &terminal{fd: int(os.Stdin.Fd())}
+func newTerminal() (*terminal, error) {
+	fd := int(os.Stdin.Fd())
+
+	if !term.IsTerminal(fd) {
+		return nil, errors.New("stdin is not a terminal")
+	}
+
+	w, h, err := term.GetSize(fd)
+	if err != nil {
+		return nil, err
+	}
+
+	return &terminal{fd: fd, width: w, height: h}, nil
 }
 
 func (t *terminal) enterRaw() (err error) {
-	if !term.IsTerminal(t.fd) {
-		return errors.New("stdin is not a terminal")
-	}
-
 	t.oldState, err = term.MakeRaw(t.fd)
 	return
 }
@@ -67,23 +75,55 @@ func (t *terminal) moveCursorRight(cols int) {
 	fmt.Printf("\r\x1b[%dC", cols)
 }
 
-func (t *terminal) moveCursorUp() {
-	fmt.Printf("\x1b[A")
+func (t *terminal) moveCursorUp(times int) {
+	for range times {
+		t.printf("\x1b[A")
+	}
 }
 
-type screenCfg struct {
-	clear bool
+func (t *terminal) println(a ...any) {
+	fmt.Println(a...)
 }
 
-func (s screenCfg) shouldClear() bool {
-	return s.clear
+func (t *terminal) printf(format string, a ...any) {
+	fmt.Printf(format, a...)
 }
 
+// helpers
+func runRaw(fn func(*terminal) error) error {
+	term, err := newTerminal()
+	if err != nil {
+		return err
+	}
+
+	if err := term.enterRaw(); err != nil {
+		return err
+	}
+
+	defer func() {
+		_ = term.restore()
+		fmt.Println()
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	defer signal.Stop(sig)
+
+	go func() {
+		<-sig
+		term.restore()
+		os.Exit(1)
+	}()
+
+	return fn(term)
+}
+
+// simple theming
 type Theme struct {
 	Prompt   string // e.g. "❯ "
 	Selected string // e.g. "● "
 	Cursor   string // e.g. "█ "
-	Error    string // e.g. "\033[31m" red
 }
 
 var defaultTheme = Theme{
@@ -103,11 +143,9 @@ func chooseTheme(t *Theme) Theme {
 type InputPrompt struct {
 	title, placeholder string
 	valuePtr           *string
-	mask               bool
 	validate           func(string) error
 	theme              *Theme
-
-	screenCfg
+	clearScreen        bool
 }
 
 func NewInput() *InputPrompt {
@@ -135,182 +173,193 @@ func (ip InputPrompt) Theme(t *Theme) *InputPrompt {
 }
 
 func (ip InputPrompt) ClearScreen(on bool) *InputPrompt {
-	ip.clear = on
+	ip.clearScreen = on
 	return &ip
 }
 
-func (ip InputPrompt) Run() (string, error) {
-	term := newTerminal()
+func (ip InputPrompt) Validate(fn func(string) error) *InputPrompt {
+	ip.validate = fn
+	return &ip
+}
 
-	if err := term.enterRaw(); err != nil {
-		return "", err
-	}
+func (ip *InputPrompt) Run() (string, error) {
+	var res string
 
-	defer func() {
-		_ = term.restore()
-		fmt.Println()
-	}()
+	err := runRaw(func(term *terminal) error {
+		var buf []rune
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-
-	defer signal.Stop(sig)
-
-	go func() {
-		<-sig
-		term.restore()
-		os.Exit(1)
-	}()
-
-	var buf []rune
-
-	if ip.valuePtr != nil && *ip.valuePtr != "" {
-		buf = []rune(*ip.valuePtr)
-	}
-
-	cursor := len(buf)
-
-	// render
-	for {
-		if ip.shouldClear() {
-			term.clearScreenAndTop()
+		if ip.valuePtr != nil && *ip.valuePtr != "" {
+			buf = []rune(*ip.valuePtr)
 		}
 
-		term.clearLine()
+		cursor := len(buf)
 
-		if ip.title != "" {
-			fmt.Print(ip.title + ": ")
-		}
+		// render
+		for {
+			if ip.clearScreen {
+				term.clearScreenAndTop()
+			}
 
-		if len(buf) == 0 && ip.placeholder != "" {
-			fmt.Print("\x1b[38;5;241m" + ip.placeholder + "\x1b[0m")
-		} else {
-			fmt.Print(string(buf))
-		}
+			term.clearLine()
 
-		prefix := 0
+			if ip.title != "" {
+				term.printf("%s", ip.title)
+			}
 
-		if ip.title != "" {
-			prefix = runewidth.StringWidth(ip.title) + 2
-		}
+			if len(buf) == 0 && ip.placeholder != "" {
+				term.printf("\x1b[38;5;241m%s\x1b[0m", ip.placeholder)
+			} else {
+				term.printf("%s", string(buf))
+			}
 
-		term.moveCursorRight(prefix + runewidth.StringWidth(string(buf[:cursor])))
+			prefix := 0
 
-		key, err := term.readKey()
-		if err != nil {
-			return "", err
-		}
+			if ip.title != "" {
+				prefix = runewidth.StringWidth(ip.title)
+			}
 
-		// arrow keys
-		if len(key) >= 3 && key[0] == 0x1b && key[1] == '[' {
-			switch key[2] {
-			case 'D': // left
+			term.moveCursorRight(prefix + runewidth.StringWidth(string(buf[:cursor])))
+
+			key, err := term.readKey()
+			if err != nil {
+				return err
+			}
+
+			// arrow keys
+			if len(key) >= 3 && key[0] == '\x1b' && key[1] == '[' {
+				switch key[2] {
+				case 'D': // left
+					if cursor > 0 {
+						cursor--
+					}
+					continue
+				case 'C': // right
+					if cursor < len(buf) {
+						cursor++
+					}
+					continue
+				case 'A', 'B':
+					continue
+				}
+			}
+
+			b0 := key[0]
+			switch {
+			case b0 == '\r' || b0 == '\n':
+				res := string(buf)
+
+				if ip.validate != nil {
+					if err := ip.validate(res); err != nil {
+						term.printf("\n%s\n", err.Error())
+						continue
+					}
+				}
+
+				if ip.valuePtr != nil {
+					*ip.valuePtr = res
+				}
+
+				return nil
+			case b0 == keyCtrlC, b0 == keyCtrlQ:
+				return errors.New("canceled")
+			case b0 == keyBackspace1 || b0 == keyBackspace2:
 				if cursor > 0 {
+					buf = append(buf[:cursor-1], buf[cursor:]...)
 					cursor--
 				}
-				continue
-			case 'C': // right
-				if cursor < len(buf) {
-					cursor++
+			case b0 >= 32 && b0 <= 126: // printable
+				r, size := utf8.DecodeRune(key)
+				if r == utf8.RuneError || size == 0 {
+					continue
 				}
-				continue
-			case 'A', 'B':
-				continue
-			}
-		}
 
-		b0 := key[0]
-
-		switch {
-		case b0 == '\r' || b0 == '\n':
-			res := string(buf)
-
-			if ip.valuePtr != nil {
-				*ip.valuePtr = res
-			}
-
-			return res, nil
-		case b0 == keyCtrlC, b0 == keyCtrlQ:
-			return "", errors.New("canceled")
-		case b0 == 127 || b0 == 8: // backspace
-			if cursor > 0 {
-				buf = append(buf[:cursor-1], buf[cursor:]...)
-				cursor--
-			}
-		case b0 >= 32 && b0 <= 126: // printable
-			r, _ := utf8.DecodeRune(key)
-
-			if r != utf8.RuneError {
 				buf = append(buf[:cursor], append([]rune{r}, buf[cursor:]...)...)
 				cursor++
 			}
 		}
+	})
+
+	if err != nil {
+		return "", err
 	}
+
+	return res, nil
 }
 
 // Confirm
 type Confirm struct {
-	question string
-	def      bool
-
-	screenCfg
+	question         string
+	def, clearScreen bool
 }
 
-func NewConfirm(q string) *Confirm {
-	return &Confirm{question: q}
+func NewConfirm() *Confirm {
+	return &Confirm{}
+}
+
+func (c Confirm) Question(q string) *Confirm {
+	c.question = q
+	return &c
 }
 
 func (c Confirm) ClearScreen(on bool) *Confirm {
-	c.clear = on
+	c.clearScreen = on
 	return &c
 }
 
 func (c *Confirm) Run() (bool, error) {
-	term := newTerminal()
-	defer term.restore()
+	var res bool
 
-	df := "y/N"
+	err := runRaw(func(t *terminal) error {
+		df := "y/N"
 
-	if c.def {
-		df = "Y/n"
+		if c.def {
+			df = "Y/n"
+		}
+
+		for {
+			if c.clearScreen {
+				t.clearScreenAndTop()
+			}
+
+			t.clearLine()
+			t.printf("%s [%s]", c.question, df)
+
+			key, err := t.readKey()
+			if err != nil {
+				return err
+			}
+
+			switch b := key[0]; b {
+			case keyCtrlC, keyCtrlQ:
+				return errors.New("canceled")
+			case '\r', '\n':
+				res = c.def
+				return nil
+			case 'y', 'Y':
+				res = true
+				return nil
+			case 'n', 'N':
+				res = false
+				return nil
+			default:
+				return errors.New("invalid input")
+			}
+		}
+	})
+
+	if err != nil {
+		return c.def, err
 	}
 
-	for {
-		if c.shouldClear() {
-			term.clearScreenAndTop()
-		}
-
-		term.clearLine()
-
-		fmt.Printf("%s [%s]: ", c.question, df)
-
-		key, err := term.readKey()
-		if err != nil {
-			return c.def, err
-		}
-
-		switch b := key[0]; b {
-		case keyCtrlC, keyCtrlQ:
-			return c.def, errors.New("canceled")
-		case '\r', '\n':
-			return c.def, nil
-		case 'y', 'Y':
-			return true, nil
-		case 'n', 'N':
-			return false, nil
-		default:
-			return c.def, errors.New("invalid input")
-		}
-	}
+	return res, nil
 }
 
 // select
 type Select struct {
-	title   string
-	options []string
-	theme   *Theme
-
-	screenCfg
+	title      string
+	options    []string
+	theme      *Theme
+	clearSreen bool
 }
 
 func NewSelect() *Select {
@@ -327,95 +376,86 @@ func (s Select) Options(o []string) *Select {
 	return &s
 }
 
+func (s Select) Theme(t *Theme) *Select {
+	s.theme = t
+	return &s
+}
+
 func (s Select) ClearScreen(on bool) *Select {
-	s.clear = on
+	s.clearSreen = on
 	return &s
 }
 
 func (s *Select) Run() (string, error) {
-	term := newTerminal()
-	theme := chooseTheme(s.theme)
+	if len(s.options) == 0 {
+		return "", errors.New("no options")
+	}
 
-	if err := term.enterRaw(); err != nil {
+	var res string
+
+	err := runRaw(func(t *terminal) error {
+		cursor := 0
+		theme := chooseTheme(s.theme)
+
+		if s.title != "" {
+			t.println(s.title)
+		}
+
+		for {
+			if s.clearSreen {
+				t.clearScreenAndTop()
+			} else {
+				t.clearLine()
+			}
+
+			for i, opt := range s.options {
+				prefix := "  "
+
+				if i == cursor {
+					prefix = theme.Prompt
+				}
+
+				t.printf("\r%s%s\n", prefix, opt)
+			}
+
+			key, err := t.readKey()
+			if err != nil {
+				return err
+			}
+
+			switch string(key) {
+			case keyArrowUp:
+				if cursor > 0 {
+					cursor--
+				}
+			case keyArrowDown:
+				if cursor < len(s.options)-1 {
+					cursor++
+				}
+			case keyEnter:
+				res = s.options[cursor]
+				return nil
+			case string([]byte{keyCtrlC}):
+				return errors.New("canceled")
+			}
+
+			t.moveCursorUp(len(s.options))
+		}
+	})
+
+	if err != nil {
 		return "", err
 	}
 
-	defer func() {
-		_ = term.restore()
-		fmt.Println()
-	}()
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-
-	defer signal.Stop(sig)
-
-	go func() {
-		<-sig
-		term.restore()
-		os.Exit(1)
-	}()
-
-	cursor := 0
-
-	if s.title != "" {
-		fmt.Println(s.title)
-	}
-
-	for {
-		// render
-		if s.shouldClear() {
-			term.clearScreenAndTop()
-		} else {
-			term.clearLine()
-		}
-
-		for i, opt := range s.options {
-			fmt.Printf("\r") // force cursor to 0
-			prefix := "  "
-
-			if i == cursor {
-				prefix = theme.Prompt
-			}
-
-			fmt.Printf("%s%s\n", prefix, opt)
-		}
-
-		key, err := term.readKey()
-		if err != nil {
-			return "", err
-		}
-
-		if len(key) == 1 && key[0] == keyCtrlC {
-			return "", errors.New("canceled")
-		}
-
-		switch string(key) {
-		case keyArrowUp:
-			if cursor > 0 {
-				cursor--
-			}
-		case keyArrowDown:
-			if cursor < len(s.options)-1 {
-				cursor++
-			}
-		case keyEnter:
-			return s.options[cursor], nil
-		}
-
-		for range len(s.options) {
-			term.moveCursorUp()
-		}
-	}
+	return res, nil
 }
 
 // Multi-select
 type MultiSelect struct {
-	title   string
-	options []string
-	theme   *Theme
-
-	screenCfg
+	title       string
+	options     []string
+	theme       *Theme
+	clearScreen bool
 }
 
 func NewMultiSelect() *MultiSelect {
@@ -432,101 +472,90 @@ func (m MultiSelect) Options(o []string) *MultiSelect {
 	return &m
 }
 
+func (m MultiSelect) Theme(t *Theme) *MultiSelect {
+	m.theme = t
+	return &m
+}
+
 func (m MultiSelect) ClearScreen(on bool) *MultiSelect {
-	m.clear = on
+	m.clearScreen = on
 	return &m
 }
 
 func (m *MultiSelect) Run() ([]string, error) {
-	term := newTerminal()
-	theme := chooseTheme(m.theme)
+	if len(m.options) == 0 {
+		return nil, errors.New("no options")
+	}
 
-	if err := term.enterRaw(); err != nil {
+	var chosen []string
+
+	err := runRaw(func(t *terminal) error {
+		cursor := 0
+		selected := make([]bool, len(m.options))
+		theme := chooseTheme(m.theme)
+
+		if m.title != "" {
+			t.println(m.title)
+		}
+
+		for {
+			if m.clearScreen {
+				t.clearScreenAndTop()
+			} else {
+				t.clearLine()
+			}
+
+			for i, opt := range m.options {
+				t.printf("\r")
+				mark := "● "
+
+				if selected[i] {
+					mark = theme.Selected
+				}
+
+				prefix := "  "
+
+				if i == cursor {
+					prefix = theme.Prompt
+				}
+
+				t.printf("\r%s%s %s\n", prefix, mark, opt)
+			}
+
+			key, err := t.readKey()
+			if err != nil {
+				return err
+			}
+
+			switch string(key) {
+			case keyArrowUp:
+				if cursor > 0 {
+					cursor--
+				}
+			case keyArrowDown:
+				if cursor < len(m.options)-1 {
+					cursor++
+				}
+			case keySpace:
+				selected[cursor] = !selected[cursor]
+			case keyEnter:
+				for i, v := range selected {
+					if v {
+						chosen = append(chosen, m.options[i])
+					}
+				}
+				return nil
+			case string([]byte{keyCtrlC}):
+				return errors.New("canceled")
+			}
+
+			t.moveCursorUp(len(m.options))
+		}
+	})
+
+	if err != nil {
 		return nil, err
 	}
 
-	defer func() {
-		_ = term.restore()
-		fmt.Println()
-	}()
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-
-	defer signal.Stop(sig)
-
-	go func() {
-		<-sig
-		term.restore()
-		os.Exit(1)
-	}()
-
-	cursor := 0
-	selected := make([]bool, len(m.options))
-
-	if m.title != "" {
-		fmt.Println(m.title)
-	}
-
-	for {
-		// render
-		if m.shouldClear() {
-			term.clearScreenAndTop()
-		}
-
-		term.clearLine()
-
-		for i, opt := range m.options {
-			fmt.Printf("\r") // force to 0
-			mark := " "
-
-			if selected[i] {
-				mark = theme.Selected
-			}
-
-			prefix := "  "
-
-			if i == cursor {
-				prefix = theme.Prompt
-			}
-
-			fmt.Printf("%s%s %s\n", prefix, mark, opt)
-		}
-
-		key, err := term.readKey()
-		if err != nil {
-			return nil, err
-		}
-
-		if len(key) == 1 && key[0] == keyCtrlC {
-			return nil, errors.New("canceled")
-		}
-
-		switch string(key) {
-		case keyArrowUp:
-			if cursor > 0 {
-				cursor--
-			}
-		case keyArrowDown:
-			if cursor < len(m.options)-1 {
-				cursor++
-			}
-		case keySpace:
-			selected[cursor] = !selected[cursor]
-		case keyEnter:
-			var chosen []string
-
-			for i, v := range selected {
-				if v {
-					chosen = append(chosen, m.options[i])
-				}
-			}
-
-			return chosen, nil
-		}
-
-		for range len(m.options) {
-			term.moveCursorUp()
-		}
-	}
+	return chosen, nil
 }
